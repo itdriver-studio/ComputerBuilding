e6cc5dfb673f0994d93406a835347d63175dbf6f
Dmitrii Ivanov <itdriver@yahoo.com>
2022-11-17 16:14:34 +0100

    header adaptive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 let total = +document.querySelector('.header__progressbar-num--end').innerHTML
 let income = +document.querySelector('.header__progressbar-text--num').innerHTML
 let scale = document.querySelector('.header__progressbar-scale');
+let adaptiveMenu = document.querySelector('.nav__list-inner');
+let burger = document.querySelector('.nav__list-menuimg');
 
 scaleWidth = income / (total / 100);
 
 scale.style.width = scaleWidth + '%';
+
+burger.onclick = () => {
+  adaptiveMenu.classList.toggle('nav__list-inner--adaptive')
+}
